b6d2bd7b71fe7108b5e5b9918f8767df91f1b3be
Kyler Marquis <cryptojedi@MacBook-Pro-2.local>
2022-09-13 15:04:28 -0400

    added error.ejs, enabled genres again

--- controllers/anime.js
+++ controllers/anime.js
@@ -26,15 +26,29 @@ router.get('/', (req, res) => {
 
 //SHOW
 router.get('/:id', (req, res) =>
-    fetch(`https://api.jikan.moe/v4/anime/${req.params.id}`)
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data)
-        res.render('anime/show.ejs', {
-            anime: data,
-            // genres: data.data.genres.map(g => g.name).join(', '), //maps out the .name values to the g array
-        })
+{const url = `https://api.jikan.moe/v4/anime/${req.params.id}`;
+fetch(url)
+    .then((response) => {
+        // if our response from the API is NOT okay:
+        if (!response.ok) {
+            throw new Error('Failed to fetch!');
+        }
+
+        return response.json();
     })
-)
+    .then((json) => {
+        res.render('anime/show.ejs', {
+            anime: json,
+            genres: json.data.genres.map(g => g.name).join(', '), //maps out the .name values to the g array
+        })
+    }).catch(err => {
+        //handles errors by rendering error page:
+        console.error('Error', err);
+        res.render('anime/error.ejs', {
+            id: req.params.id,
+            url,
+        });
+    });
+});
 
 module.exports = router
