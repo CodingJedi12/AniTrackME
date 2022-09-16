2baec13286ed323d91265640a04c9875e1025fe2
Kyler Marquis <cryptojedi@MacBook-Pro-2.local>
2022-09-16 18:40:40 -0400

    removed node-fetch from planToWatch.js

--- controllers/planToWatch.js
+++ controllers/planToWatch.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const router = express.Router();
 const planToWatch = require('../models/planToWatch.js');
-const fetch = require('node-fetch');
 
 //INDEX
 router.get('/', (req, res) => {
